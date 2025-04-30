f2e25c64a5f2154cd70e660900a95f5d599004e8
nealchampagne <166440916+nealchampagne@users.noreply.github.com>
2025-04-30 12:59:09 -0400

    Create links for SPA navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,39 @@
 import { Link } from "react-router-dom";
+import { useState, useContext, useEffect } from "react";
 import cart from "../assets/cart.svg";
+import styles from '../styles/Navbar.module.css';
+import { CartContext } from '../App';
 
 const Navbar = () => {
+
+  const { cartArray, setCartArray } = useContext(CartContext);
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    let newCartArray = cartArray;
+    let newCartCount = cartArray.length;
+    setCartCount(newCartCount);
+  }, [cartArray]);
+
   return (
-    <>
-      <div>
-        <h1 className="title">Store</h1>
-        <img />
+    <header>
+      <div className={styles.accentbar}></div>
+      <div className={styles.main}>
+        <div className={styles.namecontainer}>
+          <h1 className={styles.name}>KON·SUMP·SHON</h1>
+          <img className={styles.logo} href={null} />
+        </div>
+        <nav>
+          <Link to="/" className={styles.pagelink}>Home</Link>
+          <Link to="/shop" className={styles.pagelink}>Shop</Link>
+        </nav>
+        <div className={styles.cartcontainer}>
+          <Link to="/cart" className={styles.carticon}><img src={cart}/></Link>
+          <div className={styles.cartcount}>{cartCount}</div>
+        </div>
       </div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/shop">Shop</Link>
-        <Link to="/cart"><img src={cart}/></Link>
-      </nav>
-    </>
-  )
+    </header>
+  );
 };
 
 export default Navbar;
