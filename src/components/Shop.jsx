c8027629fd5e7d0249973444b9239f9f69878646
nealchampagne <166440916+nealchampagne@users.noreply.github.com>
2025-05-09 17:15:18 -0400

    Implement Shop component

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -2,7 +2,9 @@ import { useState, useEffect, useContext } from 'react';
 import ShopItem from './ShopItem';
 import styles from '../styles/Shop.module.css'
 import { CartContext } from '../App';
-
+import ShimmerCard from './ShimmerCard';
+import { v4 as uuidv4 } from 'uuid';
+import ErrorPage from './ErrorPage';
 
 const Shop = () => {
   
@@ -20,17 +22,24 @@ const Shop = () => {
       }
       return response.json();
     })
+    .then(setTimeout(() => {}, 1000))
     .then((response) => setCatalog(response))
     .catch((error) => setError(error))
     .finally(() => setLoading(false));
   }, []);
 
+  const shimmerArray = new Array(20);
+  shimmerArray.fill(0);
+
   return (
-    <div className={styles.shop}>
-      {catalog.map(item => <ShopItem key={item.id} id={item.id} title={item.title}
-        price={item.price} image={item.image} rating={item.rating} />
-      )}
-    </div>
+    <>
+      {error ? <ErrorPage error={error}/> : <div className={styles.shop}>
+        {loading ? shimmerArray.map(item => <ShimmerCard key={uuidv4()} type='shop'/>)
+        : catalog.map(item => <ShopItem key={item.id} id={item.id} title={item.title}
+          price={item.price} image={item.image} rating={item.rating} />
+        )}
+      </div>}
+    </>
   );
 };
 
