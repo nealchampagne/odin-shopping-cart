22080a2b1f8048e75a20595c001089dad16c4ceb
nealchampagne <166440916+nealchampagne@users.noreply.github.com>
2025-05-09 17:16:56 -0400

    Implement ShimmerCard component for loading

---
+++ src/components/ShimmerCard.jsx
@@ -0,0 +1,29 @@
+import styles from '../styles/ShimmerCard.module.css'
+
+const ShimmerCard = ({ type }) => {
+
+  return (
+    <>
+      {type === 'shop' ? 
+        <div className={styles.shopCard}>
+          <div className={styles.shimmer + ' ' + styles.title}></div>
+          <div className={styles.shimmer + ' ' + styles.titleEnd}></div>
+          <div className={styles.shimmer + ' ' + styles.shopImage}></div>
+          <div className={styles.shimmer + ' ' + styles.content}></div>
+          <div className={styles.shimmer + ' ' + styles.button}></div>
+        </div>
+      : <div className={styles.cartCard}>
+          <div className={styles.shimmer + ' ' + styles.cartImage}></div>
+          <div className={styles.info}>
+            <div className={styles.shimmer + ' ' + styles.title}></div>
+            <div className={styles.shimmer + ' ' + styles.titleEnd}></div>
+            <div className={styles.shimmer + ' ' + styles.content}></div>
+            <div className={styles.shimmer + ' ' + styles.button}></div>
+          </div>
+        </div>
+      } 
+    </>  
+  )
+}
+
+export default ShimmerCard;
