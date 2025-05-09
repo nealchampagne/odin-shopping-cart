b17cbde3b47ecaadb61d1521843553625eafe908
nealchampagne <166440916+nealchampagne@users.noreply.github.com>
2025-05-09 17:16:12 -0400

    Implement aesthetic Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,25 @@
+import purplevisor from '../assets/purplevisor.jpg'
+import purplesneakers from '../assets/purplesneakers.jpg'
+import purpleglasses from '../assets/purpleglasses.jpg'
+import flowerpocket from '../assets/flowerpocket.jpg'
+import styles from '../styles/Home.module.css'
+import { useState } from 'react';
+
 const Home = () => {
- return (
-  <h1>HOME!</h1>
- )
+
+  const [loaded, setLoaded] = useState(true);
+
+  return (
+    <div className={loaded ? styles.home : styles.hidden}>
+      <h1 className={styles.title}>KON · SUMP · SHON</h1>
+      <div className={styles.imagecontainer}>
+        <img className={styles.image + ' ' + styles.one} src={purpleglasses} />
+        <img className={styles.image + ' ' + styles.two} src={purplevisor} />
+        <img className={styles.image + ' ' + styles.three} src={purplesneakers} />
+        <img className={styles.image + ' ' + styles.four} src={flowerpocket} />
+      </div>
+    </div>
+  )
 }
 
 export default Home;
