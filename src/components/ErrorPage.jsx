c5a485de56b30c9952fb967ea2048da430a235c2
nealchampagne <166440916+nealchampagne@users.noreply.github.com>
2025-05-22 18:48:08 -0400

    Finalize ErrorPage component

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
-import styles from '../styles/ErrorPage.module.css'
+import styles from "../styles/ErrorPage.module.css";
 
-const ErrorPage = ({error}) => {
+// Render any errors in a nice format and provide links back to safety
+const ErrorPage = ({ error }) => {
   return (
     <div className={styles.errorPage}>
       <h1 className={styles.errorHeading}>Error:</h1>
-      <h2 className={styles.errorMessage}>{error ? `${error}` 
-      : `This route doesn't exist!`}</h2>
+      <h2 data-testid="errormessage" className={styles.errorMessage}>
+        {error ? `${error}` : `This route doesn't exist!`}
+      </h2>
       <Link className={styles.rescueLink} to="/">
         Return to home page
       </Link>
