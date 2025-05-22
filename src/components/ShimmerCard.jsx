1a6e1b3ac19d1bb6ed0413576b8ab698d11f7d54
nealchampagne <166440916+nealchampagne@users.noreply.github.com>
2025-05-22 18:49:05 -0400

    Finalize ShimmerCard component implementation

--- src/components/ShimmerCard.jsx
+++ src/components/ShimmerCard.jsx
@@ -1,29 +1,30 @@
-import styles from '../styles/ShimmerCard.module.css'
+import styles from "../styles/ShimmerCard.module.css";
 
 const ShimmerCard = ({ type }) => {
-
   return (
+    // Show shop or cart item shimmer configuration based on "type" prop
     <>
-      {type === 'shop' ? 
+      {type === "shop" ? (
         <div className={styles.shopCard}>
-          <div className={styles.shimmer + ' ' + styles.title}></div>
-          <div className={styles.shimmer + ' ' + styles.titleEnd}></div>
-          <div className={styles.shimmer + ' ' + styles.shopImage}></div>
-          <div className={styles.shimmer + ' ' + styles.content}></div>
-          <div className={styles.shimmer + ' ' + styles.button}></div>
+          <div className={styles.shimmer + " " + styles.title}></div>
+          <div className={styles.shimmer + " " + styles.titleEnd}></div>
+          <div className={styles.shimmer + " " + styles.shopImage}></div>
+          <div className={styles.shimmer + " " + styles.content}></div>
+          <div className={styles.shimmer + " " + styles.button}></div>
         </div>
-      : <div className={styles.cartCard}>
-          <div className={styles.shimmer + ' ' + styles.cartImage}></div>
+      ) : (
+        <div className={styles.cartCard}>
+          <div className={styles.shimmer + " " + styles.cartImage}></div>
           <div className={styles.info}>
-            <div className={styles.shimmer + ' ' + styles.title}></div>
-            <div className={styles.shimmer + ' ' + styles.titleEnd}></div>
-            <div className={styles.shimmer + ' ' + styles.content}></div>
-            <div className={styles.shimmer + ' ' + styles.button}></div>
+            <div className={styles.shimmer + " " + styles.title}></div>
+            <div className={styles.shimmer + " " + styles.titleEnd}></div>
+            <div className={styles.shimmer + " " + styles.content}></div>
+            <div className={styles.shimmer + " " + styles.button}></div>
           </div>
         </div>
-      } 
-    </>  
-  )
-}
+      )}
+    </>
+  );
+};
 
 export default ShimmerCard;
