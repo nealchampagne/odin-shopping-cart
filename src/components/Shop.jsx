f8183c77da7b6e5f12b6c102c8d3f3ef3006f0bc
nealchampagne <166440916+nealchampagne@users.noreply.github.com>
2025-05-22 18:49:36 -0400

    Finalize shop page UX and logic

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,45 +1,61 @@
-import { useState, useEffect, useContext } from 'react';
-import ShopItem from './ShopItem';
-import styles from '../styles/Shop.module.css'
-import { CartContext } from '../App';
-import ShimmerCard from './ShimmerCard';
-import { v4 as uuidv4 } from 'uuid';
-import ErrorPage from './ErrorPage';
+import { useState, useEffect } from "react";
+import ShopItem from "./ShopItem";
+import styles from "../styles/Shop.module.css";
+import ShimmerCard from "./ShimmerCard";
+import { v4 as uuidv4 } from "uuid";
+import ErrorPage from "./ErrorPage";
 
 const Shop = () => {
-  
+
+  // Initialize state variables
   const [catalog, setCatalog] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  const { cartArray, setCartArray } = useContext(CartContext);
-
+  // Fetch catalog data and store in catalog state
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products', { mode: "cors" },)
-    .then((response) => {
-      if (response.status >= 400) {
-        throw new Error("server error");
-      }
-      return response.json();
-    })
-    .then(setTimeout(() => {}, 1000))
-    .then((response) => setCatalog(response))
-    .catch((error) => setError(error))
-    .finally(() => setLoading(false));
+    fetch("https://fakestoreapi.com/products", { mode: "cors" })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("server error");
+        }
+        return response.json();
+      })
+      .then((response) => setCatalog(response))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
   }, []);
 
+  // Create array for shimmer cards
   const shimmerArray = new Array(20);
   shimmerArray.fill(0);
 
   return (
-    <>
-      {error ? <ErrorPage error={error}/> : <div className={styles.shop}>
-        {loading ? shimmerArray.map(item => <ShimmerCard key={uuidv4()} type='shop'/>)
-        : catalog.map(item => <ShopItem key={item.id} id={item.id} title={item.title}
-          price={item.price} image={item.image} rating={item.rating} />
-        )}
-      </div>}
-    </>
+    <div data-testid="shop">
+      {/* If there's an error, show the error page */}
+      {error ? (
+        <ErrorPage error={error} />
+      ) : (
+        // Otherwise, show the shop page
+        <div data-testid="shopcards" className={styles.shop}>
+          {/* While loading, show the shimmer cards */}
+          {loading
+            ? shimmerArray.map((item) => (
+                <ShimmerCard key={uuidv4()} type="shop" />
+              ))
+            // Once done loading, render the real shop items
+            : catalog.map((item) => (
+                <ShopItem
+                  key={item.id}
+                  id={item.id}
+                  title={item.title}
+                  price={item.price}
+                  image={item.image}
+                />
+              ))}
+        </div>
+      )}
+    </div>
   );
 };
 
--- src/components/ShopItem.jsx
+++ src/components/ShopItem.jsx
@@ -1,31 +1,38 @@
-import { useState, useContext } from 'react';
-import styles from '../styles/ShopItem.module.css'
-import { CartContext } from '../App';
+import { useState, useContext } from "react";
+import styles from "../styles/ShopItem.module.css";
+import CartContext from "./CartContext";
 
+const ShopItem = ({ id, title, price, image }) => {
 
-const ShopItem = ({id, title, price, image, description, rating}) => {
-  
+  // Initialize state and use CartContext
   const [count, setCount] = useState(0);
-  const { cartArray, setCartArray }  = useContext(CartContext);
+  const { cartArray, setCartArray } = useContext(CartContext);
 
-  const handleUpdateCount = num => {
+  // Update the count on the shop item
+  const handleUpdateCount = (num) => {
     if (num > 0) {
       setCount(parseInt(count) + num);
     } else if (count > 0) {
       setCount(parseInt(count) + num);
-    };
+    }
   };
 
+  /** Add a new item of the given quantity to the cart array,
+   *  or update the item count if the item already exists in the cart */
   const handleAddItem = () => {
     let cartCopy = [...cartArray];
     if (count !== 0) {
-      if (cartCopy.length === 0 || !cartCopy.find(e => e.id === id)) {
-        cartCopy.splice(0, 0, { id: id, count: count, cost: price * count })
+      if (cartCopy.length === 0 || !cartCopy.find((e) => e.id === id)) {
+        cartCopy.splice(0, 0, { id: id, count: count, cost: price * count });
         setCartArray(cartCopy);
         setCount(0);
       } else {
-        let oldElement = cartCopy.find(e => e.id === id);
-        cartCopy.splice(cartCopy.indexOf(oldElement), 1, { id: oldElement.id, count: oldElement.count + count, cost: oldElement.cost + (price * count)})
+        let oldElement = cartCopy.find((e) => e.id === id);
+        cartCopy.splice(cartCopy.indexOf(oldElement), 1, {
+          id: oldElement.id,
+          count: oldElement.count + count,
+          cost: oldElement.cost + price * count,
+        });
         setCartArray(cartCopy);
         setCount(0);
       }
@@ -35,17 +42,68 @@ const ShopItem = ({id, title, price, image, description, rating}) => {
   return (
     <div className={styles.shopCard}>
       <div className={styles.cardtop}>
-        <p tabIndex="0" aria-label="title" className={styles.title}>{title}</p>
-        <p tabIndex="0" aria-label="price" className={styles.price}>${parseFloat(price).toFixed(2)}</p>
+        <p
+          id={`title${id}`}
+          tabIndex="0"
+          aria-label="title"
+          className={styles.title}
+        >
+          {title}
+        </p>
+        <p
+          id={`price${id}`}
+          tabIndex="0"
+          aria-label="price"
+          className={styles.price}
+        >
+          ${parseFloat(price).toFixed(2)}
+        </p>
       </div>
-      <img tabIndex="0" alt={title} className={styles.image} src={image} />
+      <img
+        id={`image${id}`}
+        tabIndex="0"
+        alt={title}
+        className={styles.image}
+        src={image}
+      />
       <div className={styles.cardbottom}>
         <div className={styles.quantity}>
-          <button aria-label="decrement item"className={styles.decrement} onClick={() => handleUpdateCount(-1)}>-</button>
-          <input aria-label="quantity" className={styles.input} type="number" value={count} onChange={e => (e.target.value >= 0 && e.target.value !== '') ? setCount(Number(e.target.value).toString()) : setCount(0)}/>
-          <button aria-label="increment item" className={styles.increment} onClick={() => handleUpdateCount(1)}>+</button>
+          <button
+            data-testid={`minus${id}`}
+            aria-label="decrement item"
+            className={styles.decrement}
+            onClick={() => handleUpdateCount(-1)}
+          >
+            -
+          </button>
+          <input
+            id={`quantity${id}`}
+            aria-label="quantity"
+            className={styles.input}
+            type="number"
+            value={count}
+            onChange={(e) =>
+              e.target.value >= 0 && e.target.value !== ""
+                ? setCount(Number(e.target.value).toString())
+                : setCount(0)
+            }
+          />
+          <button
+            data-testid={`plus${id}`}
+            aria-label="increment item"
+            className={styles.increment}
+            onClick={() => handleUpdateCount(1)}
+          >
+            +
+          </button>
         </div>
-        <button className={styles.addtocart} onClick={handleAddItem}>Add to Cart</button>
+        <button
+          data-testid="addtocart"
+          className={styles.addtocart}
+          onClick={handleAddItem}
+        >
+          Add to Cart
+        </button>
       </div>
     </div>
   );
